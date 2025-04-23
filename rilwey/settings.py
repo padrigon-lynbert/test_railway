0c308c1b8c653f3b9c73b692f3a316e9d5b68b8d
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2025-04-23 15:00:34 +0800

    config for prod

--- rilwey/settings.py
+++ rilwey/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,web-production-dfd4.up.railway.app').split(',')
 
 # Application definition
 
